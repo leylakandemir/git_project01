9a05d62b87bf84d0540ca0a76450be77baf0eb0a
Leyla <leylakandemir00@gmail.com>
2020-03-17 18:19:58 +0100

    sixth commit

--- src/gitpackage/GitDay2.java
+++ src/gitpackage/GitDay2.java
@@ -6,8 +6,7 @@ public class GitDay2 {
 		
 		System.out.println("Hello World!");
 		System.out.println("Hello Git Users");
-
-
+		System.out.println("cHECK ÝF YOUR LOCAL ÝS AHEAD OF ORÝGÝN MASTER");
 	}
 
 }
