1bd15ef3390e3f417549c7bb2f85cd89f56fc622
Leyla <leylakandemir00@gmail.com>
2020-03-17 18:54:31 +0100

    commit 10

--- src/gitpackage/NextClass.java
+++ src/gitpackage/NextClass.java
@@ -4,7 +4,7 @@ public class NextClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello Dear Friends");
+		System.out.println("Hello Dear Testers");
 
 	}
 
