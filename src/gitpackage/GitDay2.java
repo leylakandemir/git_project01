e5de3701fd6130c753ce9185e23c2ad372994b31
Leyla <leylakandemir00@gmail.com>
2020-03-17 12:26:32 +0100

    first commit

---
+++ src/gitpackage/GitDay2.java
@@ -0,0 +1,10 @@
+package gitpackage;
+
+public class GitDay2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/gitpackage/NextClass.java
@@ -0,0 +1,10 @@
+package gitpackage;
+
+public class NextClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
